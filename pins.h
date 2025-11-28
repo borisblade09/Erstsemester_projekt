d34bc40865f6ec5be87c324aac76c52f2c6218be
Thomas Limbrunner <50096128+Limbrunner@users.noreply.github.com>
2025-11-28 18:04:44 +0000

    Update pins.h
    - changed Pins

--- pins.h
+++ pins.h
@@ -6,12 +6,13 @@
 
 const int Motor_links_A_Pin = 3;  // Analog power supply
 const int Motor_links_B_Pin = 2;  // Analog power supply
-const int Motor_rechts_A_Pin = 19; // GPI012
-const int Motor_rechts_B_Pin = 18; // GPI011
+const int Motor_rechts_A_Pin = 19;
+const int Motor_rechts_B_Pin = 18; 
 
 // const int Line_Sensor_Pin = 4;    
-const int Solenoid_Pin = 27;   // GPI020   
+const int Solenoid_Pin = 20;   // GPI020   
 
 // QT Pins
-const int sensorVornePin = 4;    // GPI00
-const int sensorHintenPin = 5;  // GPI01
+const int sensorVornePin = 0;    // GPI00
+const int sensorHintenPin = 1;  // GPI01
+
